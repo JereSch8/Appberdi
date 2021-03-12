ac00f5aa97c5984c1633f78aa9527dbf4cc3429f
Jere Schneider <jereschneider8@gmail.com>
2021-03-12 01:07:46 -0300

    Se agrega:
    *App Module
    - Dependencia de material
    - Recursos graficos
    
    Se modifica:
    
    *App Module
    - Pantalla de Mapa
    
    Se elimina:
    - Importaciones deprecadas "lifecycle.observe"

--- app/src/main/java/com/jackemate/appberdi/ui/map/MapsActivity.kt
+++ app/src/main/java/com/jackemate/appberdi/ui/map/MapsActivity.kt
@@ -1,9 +1,10 @@
 package com.jackemate.appberdi.ui.map
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.observe
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -12,7 +13,6 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.firestore.GeoPoint
 import com.jackemate.appberdi.R
-import com.jackemate.appberdi.databinding.ActivityMainBinding
 import com.jackemate.appberdi.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -25,9 +25,21 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val binding  = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.cvHeader.setCardBackgroundColor(Color.TRANSPARENT)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
+        binding.btnIndications.setOnClickListener{
+            Log.e( "onCreate: ", "PRESS")
+        }
+
+        binding.btnBack.setOnClickListener {
+            Log.e( "onCreate: ", "back")
+        }
+
     }
 
 
@@ -40,9 +52,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 val landmark = LatLng(geoPoint.latitude, geoPoint.longitude)
                 mMap.addMarker(MarkerOptions().position(landmark).title(site.name))
                 mMap.setContentDescription(site.description)
-
-                if(site.name.contains("Plaza Colón"))
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(landmark))
+                mMap.setMaxZoomPreference(100.0f)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(landmark, 14.5f))
             }
         }
         // Actualizamos
