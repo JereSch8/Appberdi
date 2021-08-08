a8b4100aae11faa74c72748f392ad482dab86acf
Silva Alejandro Ismael <silva.alejandro.ismael@gmail.com>
2021-08-08 16:18:30 -0300

    Fix ObjectOutputStream.java
    De Firebase Crashlytics:
    
    Fatal Exception: java.lang.RuntimeException: Parcelable encountered IOException writing serializable object (name = com.jackemate.appberdi.entities.Content$Summary)

--- app/src/main/java/com/jackemate/appberdi/entities/SummaryItem.kt
+++ app/src/main/java/com/jackemate/appberdi/entities/SummaryItem.kt
@@ -1,18 +1,20 @@
 package com.jackemate.appberdi.entities
 
+import java.io.Serializable
+
 data class SummaryCountable(
     val count: Int? = null,
     val title: String = "",
     val subtitle: String = "",
-)
+) : Serializable
 
 data class SummaryYear(
     val title: String = "",
     val from: String = "",
     val to: String = "",
-)
+) : Serializable
 
 data class SummaryLabelIcon(
     val title: String = "",
     val icon: String = "", /* Se ignora por el momento */
-)
+) : Serializable
