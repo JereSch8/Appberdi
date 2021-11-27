931c9a14d152bcc63ace11349bccb5fa83f52dcc
Silva Alejandro Ismael <silva.alejandro.ismael@gmail.com>
2021-11-27 19:16:15 -0300

    Scroll en los filtros de mediateca
    * Usar binding en el Adapter
    * Fixes en la animación de lottie

--- app/src/main/java/com/jackemate/appberdi/ui/mediateca/MediatecaSiteAdapter.kt
+++ app/src/main/java/com/jackemate/appberdi/ui/mediateca/MediatecaSiteAdapter.kt
@@ -2,12 +2,11 @@ package com.jackemate.appberdi.ui.mediateca
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
+import android.view.MotionEvent
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.airbnb.lottie.LottieAnimationView
 import com.jackemate.appberdi.R
+import com.jackemate.appberdi.databinding.ItemMediatecaSiteBinding
 import com.jackemate.appberdi.entities.Content
 import java.lang.Integer.max
 
@@ -15,18 +14,15 @@ class MediatecaSiteAdapter(
     private val context: Context,
     private var listMultimedia: List<Content>,
     private val listener: (Content) -> Unit
-) : RecyclerView.Adapter<BaseViewHolder<*>>() {
+) : RecyclerView.Adapter<MediatecaSiteAdapter.Holder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
-        return MultiMediaViewHolder(
-            LayoutInflater.from(context).inflate(R.layout.item_mediateca_site, parent, false)
-        )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding = ItemMediatecaSiteBinding.inflate(LayoutInflater.from(context), parent, false)
+        return Holder(binding)
     }
 
-    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
-        when (holder) {
-            is MultiMediaViewHolder -> holder.bind(listMultimedia[position])
-        }
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(listMultimedia[position])
     }
 
     fun updateItems(list: List<Content>) {
@@ -39,10 +35,12 @@ class MediatecaSiteAdapter(
         return listMultimedia.size
     }
 
-    inner class MultiMediaViewHolder(itemView: View) : BaseViewHolder<Content>(itemView) {
-        override fun bind(item: Content) {
-            itemView.findViewById<TextView>(R.id.title).text = item.title
-            itemView.findViewById<TextView>(R.id.description).text = when (item) {
+    inner class Holder(val binding: ItemMediatecaSiteBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Content) {
+            binding.title.text = item.title
+            binding.description.text = when (item) {
                 is Content.Audio -> item.subtitle
                 is Content.Video -> item.description
                 is Content.Gif -> item.description
@@ -50,8 +48,9 @@ class MediatecaSiteAdapter(
                 is Content.Text -> item.description
                 else -> ""
             }
-
-            itemView.findViewById<LottieAnimationView>(R.id.typeData).setAnimation(
+            binding.typeData.cancelAnimation()
+            binding.typeData.clearAnimation()
+            binding.typeData.setAnimation(
                 when (item) {
                     is Content.Audio -> R.raw.audio
                     is Content.Video -> R.raw.video
@@ -62,7 +61,35 @@ class MediatecaSiteAdapter(
                 }
             )
 
-            itemView.findViewById<LottieAnimationView>(R.id.typeData).playAnimation()
+            /* White color
+            binding.typeData.addValueCallback(
+                KeyPath("**"),
+                LottieProperty.COLOR,
+                { ContextCompat.getColor(context, R.color.white) }
+            )
+            binding.typeData.addValueCallback(
+                KeyPath("**"),
+                LottieProperty.STROKE_COLOR,
+                { ContextCompat.getColor(context, R.color.white) }
+            )*/
+
+            // Play animation on startup
+            binding.typeData.playAnimation()
+
+            itemView.setOnTouchListener { view, event ->
+                return@setOnTouchListener when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        // Play animation on touch
+                        binding.typeData.playAnimation()
+                        false
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        view.performClick()
+                        true
+                    }
+                    else -> false
+                }
+            }
             itemView.setOnClickListener { listener(item) }
         }
 
