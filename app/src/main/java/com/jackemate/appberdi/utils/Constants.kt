d9a449097bacc7095f450358d17c75af42c1d6b4
Jere Schneider <jereschneider8@gmail.com>
2022-07-06 19:38:15 -0300

    Mejoras en la pantalla Preferencias

--- app/src/main/java/com/jackemate/appberdi/utils/Constants.kt
+++ app/src/main/java/com/jackemate/appberdi/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.jackemate.appberdi.utils
 import java.util.concurrent.TimeUnit
 
 object Constants {
-    const val GEOFENCE_RADIUS_IN_METERS = 150f
+    const val GEOFENCE_RADIUS_IN_METERS = 100f
     val GEOFENCE_EXPIRATION_IN_MILLISECONDS = TimeUnit.HOURS.toMillis(24)
 
 }
